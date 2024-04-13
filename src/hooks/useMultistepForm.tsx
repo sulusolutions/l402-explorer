84302d2129e7e9bcb403d4ff25838e95a182c617
priyanshiiit <priyanshiiit@gmail.com>
2024-04-13 13:29:08 +0530

    Adds multistep form hook

---
+++ src/hooks/useMultistepForm.tsx
@@ -0,0 +1,32 @@
+import { ReactElement, useState } from "react";
+
+export const useMultistepForm = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const next = () => {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
+  };
+
+  const back = () => {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  };
+
+  const goTo = (index: number) => {
+    setCurrentStepIndex(index);
+  };
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    steps,
+    goTo,
+    next,
+    back,
+  };
+};
